8fcf81682aaa8d214e36e47a2d7375801341e5e7
WB <13348823507@163.com>
2019-10-28 16:32:18 +0800

    配置基本的路由

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { adminRouter } from './routes';
 
-const testHOC = (WrappedComponent) => {
-  return class HOCComponent extends React.Component {
-    render () {
-      return (
-        <>
-          <WrappedComponent />
-          <div>这是高阶组件里的信息</div>
-        </>
-      )
-    }
-  }
-}
-@testHOC
+// const testHOC = (WrappedComponent) => {
+//   return class HOCComponent extends React.Component {
+//     render () {
+//       return (
+//         <>
+//           <WrappedComponent />
+//           <div>这是高阶组件里的信息</div>
+//         </>
+//       )
+//     }
+//   }
+// }
+// @testHOC
 class App extends React.Component {
   render () {
     return (
       <div>
-        <Button type = 'primary'>test</Button>
+        {/* <Button type = 'primary'>test</Button> */}
+        <Switch>
+          {
+            adminRouter.map(route => {
+              return (
+                <Route
+                  path = {route.pathname}
+                  exact = {route.exact}
+                  render = {(routeProps) => {
+                    return <route.component {...routeProps} />
+                  }}
+              />
+              )
+            })
+          }
+          <Redirect to = {adminRouter[0].pathname} from = "/admin" exact />
+          <Redirect to = "/404" />
+        </Switch>
       </div>
     )
   }
